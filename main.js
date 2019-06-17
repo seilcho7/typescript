3cc23f506a01ffe42a63d2b0b2a6b5373f894696
Seil Cho <seilcho7@hotmail.com>
2019-06-17 15:00:15 -0400

    create class and inherit from class

--- main.js
+++ main.js
@@ -5,44 +5,109 @@
 // let d: any;
 // let e: number[] = [1,2,3];
 // let f: any[] = [1,true,'a',false];
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
-//####################################################
-// const ColorRed = 0;
-// const ColorGreen = 1;
-// const ColorBlue = 2;
-// enum Color { Red = 0, Green = 1, Blue = 2 };
-// let backgroundColor = Color.Red;
-//####################################################
-// let message;
-// message = 'abc';
-// let endsWithC = (<string>message).endsWith('c');
-// let alternativeWay = (message as string).endsWith('c');
-//####################################################
-// let log = function(message) {
-//     console.log(message);
-// }
-// let doLog = (message) => console.log(message);
-//####################################################
-// class Point {
-//     constructor(private _x?: number, private _y?: number) {
-//     } 
-//     draw() {
-//         console.log('X: ' + this.x + ', y: ' + this.y);
-//     }
-//     get x() {
-//         return this.x;
-//     }
-//     set x(value) {
-//         if (value < 0)
-//             throw new Error('value cannot be less than 0.');
-//         this.x = value;
-//     }
-// }
-// let point = new Point(1, 2);
-// let x = point.x;
-// point.x = 10;
-// point.draw();
-//####################################################
-var point_1 = require("./point");
-var point = new point_1.Point(1, 2);
-point.draw();
+var message = 'Hello World';
+// console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = "Codevolution";
+var isBeginner = true;
+var total = 0;
+var name = 'Vishwas';
+// name = true;
+var sentence = "My name is " + name + ". I am a beginner in Typescript.";
+// console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Chris', 22];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 5] = "Red";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Vishwas';
+// console.log(randomValue);
+var myVariable = 10;
+function hasName(obj) {
+    return !!obj && typeof obj === 'object' && 'name' in obj;
+}
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+// let myVariable: any = 10;
+// console.log(myVariable.name);
+// myVariable();
+// (myVariable as string).toUpperCase();
+var a;
+a = 10;
+a = true;
+var b = 20;
+var multiType;
+multiType = 20;
+multiType = true;
+// function add(num1: number, num2: number = 10): number {
+function add(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+add(5, 10);
+add(5);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+};
+// fullName(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Seil');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- main.ts
+++ main.ts
@@ -58,7 +58,130 @@
 
 //####################################################
 
-import { Point } from './point';
+// import { Point } from './point';
 
-let point = new Point(1,2);
-point.draw();
+// let point = new Point(1,2);
+// point.draw();
+
+export {};
+
+let message = 'Hello World';
+// console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = "Codevolution";
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Vishwas';
+// name = true;
+
+let sentence: string = `My name is ${name}. I am a beginner in Typescript.`;
+// console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
+
+let person1: [string, number] = ['Chris', 22];
+
+enum Color {Red = 5, Green, Blue};
+let c: Color = Color.Green;
+
+let randomValue: any = 10;
+randomValue = true;
+randomValue = 'Vishwas';
+// console.log(randomValue);
+
+let myVariable: unknown = 10;
+
+function hasName(obj: any): obj is { name: string } {
+    return !!obj && typeof obj === 'object' && 'name' in obj;
+}
+
+if (hasName(myVariable)) {
+    console.log(myVariable.name);
+}
+// let myVariable: any = 10;
+// console.log(myVariable.name);
+// myVariable();
+// (myVariable as string).toUpperCase();
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+
+// function add(num1: number, num2: number = 10): number {
+function add(num1: number, num2?: number): number {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+
+add(5, 10);
+add(5);
+
+// function fullName(person: {firstName: string, lastName: string}) {
+//     console.log(`${person.firstName} ${person.lastName}`)
+// }
+
+interface Person {
+    firstName: string;
+    lastName?: string;
+}
+
+function fullName(person: Person) {
+    console.log(`${person.firstName} ${person.lastName}`)
+}
+
+let p = {
+    firstName: 'Bruce',
+    lastName: 'Wayne'
+}
+
+// fullName(p);
+
+class Employee {
+    public employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good Morning ${this.employeeName}`);
+    }
+}
+
+let emp1 = new Employee('Seil');
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName);
+    }
+    delegateWork() {
+        console.log(`Manager delegating tasks`);
+    }
+}
+
+let m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
