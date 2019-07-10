38c5ce824c5f3f598fd1fb45b7307a79be17f1d8
Seil Cho <seilcho7@hotmail.com>
2019-07-10 14:57:42 -0400

    practice typescript

---
+++ practice-1/main.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Hello World';
+console.log(message);
+var x = 20;
+var y = 20;
+var sum;
+var title = 'Codevolution';
---
+++ practice-1/main.ts
@@ -0,0 +1,9 @@
+export {};
+let message = 'Hello World';
+console.log(message);
+
+let x = 20;
+const y = 20;
+
+let sum;
+const title = 'Codevolution';
