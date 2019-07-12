156fc968600dc59047918dc82e5e3eebacc521a3
Seil Cho <seilcho7@hotmail.com>
2019-07-12 11:37:10 -0400

    learning typescript

--- practice-1/main.ts
+++ practice-1/main.ts
@@ -6,4 +6,22 @@ let x = 20;
 const y = 20;
 
 let sum;
-const title = 'Codevolution';
+const title = "Typescript"; 
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = "Seil";
+
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [1,2,3];
